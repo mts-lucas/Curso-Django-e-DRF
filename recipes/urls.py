22fdbee59a5f1ebbbd449627f6b35466d875603b
Lucas Mateus <lmateus1067@outlook.com>
2022-12-06 15:55:20 -0300

    modularizando

---
+++ recipes/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from recipes.views import contato, home, sobre
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', home),
+    path('sobre/', sobre),
+    path('contato/', contato),
+]
---
+++ recipes/views.py
@@ -0,0 +1,13 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+
+def home(request):
+    return HttpResponse('HOME')
+
+def sobre(request):
+    return HttpResponse('SOBRE')
+
+def contato(request):
+    return HttpResponse('CONTATO')
