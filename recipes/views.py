44f009aaeacce462c648cae6719f074c7b8b49a0
Lucas Mateus <lmateus1067@outlook.com>
2023-01-16 17:47:03 -0300

    limpando

--- recipes/views.py
+++ recipes/views.py
@@ -6,11 +6,3 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'recipes/home.html')
-
-
-def sobre(request):
-    return HttpResponse('SOBRE')
-
-
-def contato(request):
-    return HttpResponse('CONTATO')
