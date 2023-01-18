2f09a903dc8c3e5c62727ef189cd4946c4581d58
Lucas Mateus <lmateus1067@outlook.com>
2023-01-18 15:11:50 -0300

    criando blocks

--- projeto/settings.py
+++ projeto/settings.py
@@ -57,7 +57,10 @@ ROOT_URLCONF = 'projeto.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+
+            BASE_DIR / 'base_templates',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
