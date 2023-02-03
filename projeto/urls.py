7b07628123ad0e83037acdbf3dcbd78edae4c62b
Lucas Mateus <lmateus1067@outlook.com>
2023-02-03 16:08:58 -0300

    refistrando modelos

--- projeto/urls.py
+++ projeto/urls.py
@@ -13,11 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+# para importar arquivos nos caminhos do settings
+from django.conf import settings
+# para usar arquivos estaticos nas urls
+from django.conf.urls.static import static
 from django.contrib import admin
 # from django.http import HttpResponse
-from django.urls import path, include
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('recipes.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- recipes/models.py
+++ recipes/models.py
@@ -1,12 +1,16 @@
-from django.db import models
 from django.contrib.auth.models import User
-
+from django.db import models
 
 # Create your models here.
 
 
 class Category(models.Model):
     name = models.CharField(max_length=65)
+
+    # metodo que quando eu chamar esse objeto como string ele ira retornar o nome do objeto.
+
+    def __str__(self):
+        return self.name
 
 
 class Recipe(models.Model):
@@ -27,3 +31,6 @@ class Recipe(models.Model):
         Category, on_delete=models.SET_NULL, null=True)
     author = models.ForeignKey(
         User, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.tittle
