84cd7179b1f1a6e1f1c853da18a5e0f3e82383d9
Lucas Mateus <lmateus1067@outlook.com>
2023-02-08 16:11:42 -0300

    add querrysets

--- recipes/views.py
+++ recipes/views.py
@@ -3,13 +3,17 @@ from django.shortcuts import render
 
 from utils.recipes.factory import make_recipe
 
+from .models import Recipe
+
 # Create your views here.
 
 
 def home(request):
+
+    recipes = Recipe.objects.all().order_by('-id')
     return render(request, 'recipes/pages/home.html', context={
 
-        'recipes': [make_recipe() for _ in range(10)],
+        'recipes': recipes,
     })
 
 
@@ -18,4 +22,13 @@ def recipe(request, id):
 
         'recipe': make_recipe(),
         'is_detail_page': True,
+    })
+
+
+def category(request, category_id):
+
+    recipes = Recipe.objects.filter(category__id=category_id)
+    return render(request, 'recipes/pages/home.html', context={
+
+        'recipes': recipes,
     })
