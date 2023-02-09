bfbaef740a27d2940a12ba875cc1b30c12945159
Lucas Mateus <lmateus1067@outlook.com>
2023-02-09 17:53:31 -0300

    paginação por id

--- recipes/views.py
+++ recipes/views.py
@@ -1,7 +1,6 @@
-from django.http import Http404
-from django.shortcuts import render
+from django.shortcuts import get_list_or_404, render
 
-from utils.recipes.factory import make_recipe
+# from utils.recipes.factory import make_recipe
 
 from .models import Recipe
 
@@ -18,23 +17,30 @@ def home(request):
 
 
 def recipe(request, id):
+
+    recipe = Recipe.objects.filter(
+        pk=id,
+        is_published=True
+    ).order_by('-id').first()
+
     return render(request, 'recipes/pages/recipe-view.html', context={
 
-        'recipe': make_recipe(),
+        'recipe': recipe,
         'is_detail_page': True,
     })
 
 
 def category(request, category_id):
 
-    recipes = Recipe.objects.filter(
-        category__id=category_id, is_published=True)
+    recipes = get_list_or_404(
+        Recipe.objects.filter(
+            category__id=category_id,
+            is_published=True
+        ).order_by('-id')
+    )
 
-    if not recipes:
-        raise Http404('Not found')
-    
     return render(request, 'recipes/pages/category.html', context={
 
         'recipes': recipes,
-        'title': f'{recipes.first().category.name}',
+        'title': f'{recipes[0].category.name}',
     })
