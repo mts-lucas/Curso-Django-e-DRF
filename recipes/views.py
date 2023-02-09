a004bdec94afef50d819aaaec0f042e2361886e0
Lucas Mateus <lmateus1067@outlook.com>
2023-02-09 16:23:51 -0300

    404

--- recipes/views.py
+++ recipes/views.py
@@ -1,4 +1,4 @@
-# from django.http import HttpResponse
+from django.http import Http404
 from django.shortcuts import render
 
 from utils.recipes.factory import make_recipe
@@ -29,7 +29,12 @@ def category(request, category_id):
 
     recipes = Recipe.objects.filter(
         category__id=category_id, is_published=True)
+
+    if not recipes:
+        raise Http404('Not found')
+    
     return render(request, 'recipes/pages/category.html', context={
 
         'recipes': recipes,
+        'title': f'{recipes.first().category.name}',
     })
