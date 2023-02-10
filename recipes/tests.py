50fe7afe5fef59715745b1d0a97949bfadf51427
Lucas Mateus <lmateus1067@outlook.com>
2023-02-10 20:13:49 -0300

    primeiro teste

--- recipes/tests.py
+++ recipes/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+
+class RecipeURLsTest(TestCase):
+
+    def test_the_pytest_is_ok(self):
+        print('Ola mundo')
+        assert 1 == 1, 'Um é igual a um'
