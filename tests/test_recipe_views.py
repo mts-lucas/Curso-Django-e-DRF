50685cbc002b509a70d90d99149503991fcc58e6
Lucas Mateus <lmateus1067@outlook.com>
2023-02-13 17:51:34 -0300

    reesturturando testes

--- tests/test_recipe_views.py
+++ tests/test_recipe_views.py
@@ -1,23 +1,7 @@
 from django.test import TestCase
 from django.urls import resolve, reverse
+
 from recipes import views
-
-# Create your tests here.
-
-
-class RecipeURLsTest(TestCase):
-
-    def test_recipe_home_url_is_correct(self):
-        url = reverse('recipes:home')
-        self.assertEqual(url, '/')
-
-    def test_recipe_category_url_is_correct(self):
-        url = reverse('recipes:category', kwargs={'category_id': 1})
-        self.assertEqual(url, '/recipes/category/1/')
-
-    def test_recipe_detail_url_is_correct(self):
-        url = reverse('recipes:recipe', kwargs={'id': 1})
-        self.assertEqual(url, '/recipes/1/')
 
 
 class RecipeViewsTest(TestCase):
