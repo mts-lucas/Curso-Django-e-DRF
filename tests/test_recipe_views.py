53b4a045ccd2d5fd5018640d38d213713d39427f
Lucas Mateus <lmateus1067@outlook.com>
2023-02-14 13:56:45 -0300

    testando recipes not found

--- tests/test_recipe_views.py
+++ tests/test_recipe_views.py
@@ -21,3 +21,18 @@ class RecipeViewsTest(TestCase):
             reverse('recipes:recipe', kwargs={'id': 1})
         )
         self.assertIs(view.func, views.recipe)
+
+    def test_recipe_home_view_returns_status_code_200_OK(self):
+        response = self.client.get(reverse('recipes:home'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_recipe_home_view_loads_correct_template(self):
+        response = self.client.get(reverse('recipes:home'))
+        self.assertTemplateUsed(response, 'recipes/pages/home.html')
+
+    def test_recipe_home_template_shows_no_recipes_found_if_no_recipes(self):
+        response = self.client.get(reverse('recipes:home'))
+        self.assertIn(
+            '<h1>No recipes Found here</h1>',
+            response.content.decode('utf-8')
+        )
