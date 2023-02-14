6fd76076cace98950faed8d443df44c6295be2c2
Lucas Mateus <lmateus1067@outlook.com>
2023-02-14 17:17:15 -0300

    testes com fixtures

--- tests/test_recipe_views.py
+++ tests/test_recipe_views.py
@@ -38,7 +38,7 @@ class RecipeViewsTest(TestCase):
         recipe = Recipe.objects.create(
             category=category,
             author=author,
-            title='Recipe Title',
+            tittle='Recipe Title',
             description='Recipe Description',
             slug='recipe-slug',
             preparation_time=10,
@@ -49,7 +49,15 @@ class RecipeViewsTest(TestCase):
             preparation_steps_is_html=False,
             is_published=True,
         )
-        assert 1 == 1
+        
+        response = self.client.get(reverse('recipes:home'))
+        content = response.content.decode('utf-8')
+        response_context_recipes = response.context['recipes']
+
+        self.assertIn('Recipe Title', content)
+        self.assertIn('10 Minutos', content)
+        self.assertIn('5 Porções', content)
+        self.assertEqual(len(response_context_recipes), 1)
 
     def test_recipe_category_view_function_is_correct(self):
         view = resolve(
