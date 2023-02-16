4a53d10897ff0de818aa86e44e0d0c234cfcaef5
Lucas Mateus <lmateus1067@outlook.com>
2023-02-16 19:10:34 -0300

    teste max lenght

---
+++ tests/test_recipe_models_recipe.py
@@ -0,0 +1,21 @@
+from django.core.exceptions import ValidationError
+from parameterized import parameterized
+
+from .test_recipe_base import RecipeTestBase
+
+
+class RecipeModelTest(RecipeTestBase):
+    def setUp(self) -> None:
+        self.recipe = self.make_recipe()
+        return super().setUp()
+
+    @parameterized.expand([
+        ('tittle', 65),
+        ('description', 165),
+        ('preparation_time_unit', 65),
+        ('servings_unit', 65),
+    ])
+    def test_recipe_fields_max_length(self, field, max_length):
+        setattr(self.recipe, field, 'A' * (max_length + 1))
+        with self.assertRaises(ValidationError):
+            self.recipe.full_clean()
