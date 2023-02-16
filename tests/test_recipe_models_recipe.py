48197916e94f0ebd6f96ec70734a5477580063aa
Lucas Mateus <lmateus1067@outlook.com>
2023-02-16 20:00:51 -0300

    testando string

--- tests/test_recipe_models_recipe.py
+++ tests/test_recipe_models_recipe.py
@@ -49,3 +49,14 @@ class RecipeModelTest(RecipeTestBase):
         self.assertFalse(
             recipe.is_published,
             msg='Recipe is_published is not False',)
+
+    def test_recipe_string_representation(self):
+        needed = 'Testing Representation'
+        self.recipe.tittle = needed
+        self.recipe.full_clean()
+        self.recipe.save()
+        self.assertEqual(
+            str(self.recipe), needed,
+            msg=f'Recipe string representation must be '
+                f'"{needed}" but "{str(self.recipe)}" was received.'
+        )
