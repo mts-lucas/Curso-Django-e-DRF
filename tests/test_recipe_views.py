7c244c1f07d5285f63dcd21bca5706b0988af041
Lucas Mateus <lmateus1067@outlook.com>
2023-02-16 15:07:43 -0300

    terninando test base

--- tests/test_recipe_views.py
+++ tests/test_recipe_views.py
@@ -27,13 +27,13 @@ class RecipeViewsTest(RecipeTestBase):
         )
 
     def test_recipe_home_template_loads_recipes(self):
+        self.make_recipe()
         response = self.client.get(reverse('recipes:home'))
         content = response.content.decode('utf-8')
         response_context_recipes = response.context['recipes']
 
+        # Check if one recipe exists
         self.assertIn('Recipe Title', content)
-        self.assertIn('10 Minutos', content)
-        self.assertIn('5 Porções', content)
         self.assertEqual(len(response_context_recipes), 1)
 
     def test_recipe_category_view_function_is_correct(self):
